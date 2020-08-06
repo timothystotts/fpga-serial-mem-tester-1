795aafaa51538340c2a660d0ba9e701d3f55f199
Timothy Stotts <timothystotts08@gmail.com>
2020-08-06 14:17:19 -0400

    The script display_ascii_from_spi_spy.py was modified to execute with or with-out command-line arguments, to use an OOP model for parsing the SPI SPY text files, and to quote the 0x1B control character as '\x' in the text parse output.

--- Lab-Verification/display_ascii_from_spi_spy.py
+++ Lab-Verification/display_ascii_from_spi_spy.py
@@ -9,66 +9,158 @@ import io
 import sys
 import re
 
-EscCharacters = ["1B",]
-PartsCopi = ["c", "cp"]
-PartsCipo = ["p", "cp"]
-rexData = re.compile(r"^Data[:][ ]")
+class AnalogDiscoverySpiSpyParser:
+    EscCharacters = ["1B",]
+    PartsCopi = ["c", "cp"]
+    PartsCipo = ["p", "cp"]
+    rexData = re.compile(r"^Data[:][ ]")
+
+    def __init__(self, fileName):
+        self._currentLine = None
+        self._ioParts = None
+        self._fh = open(fileName, "r")
+  
+    def readCurrentLine(self):
+        self._currentLine = self._fh.readline()
+        if self._currentLine:
+            return True
+        else:
+            return False
+        
+    def parseDataParts(self):
+        if self._currentLine:
+            if self.rexData.match(self._currentLine):
+                dataParts = self._currentLine.split(":")
+                lineParts = dataParts[1].split(",")
+                self._ioParts = []
+                for linePart in lineParts:
+                    self._ioParts.append(linePart.split("|"))
+                return True
+            else:
+                return False
+        else:
+            return False
+    
+    def close(self):
+        self._fh.close()
+        
+    def getIoParts(self):
+        return self._ioParts
+    
+    def getIoPartsAsAscii(self):
+        self._strCopi = ""
+        self._strCipo = ""
+        cCopiEsc = []
+        cCipoEsc = []
+        self._asciiCopi = ""
+        self._asciiCipo = ""
+        
+        for ioPart in self.getIoParts():
+            if (len(ioPart) == 2):
+                cCopi = ioPart[0].strip()
+                cCipo = ioPart[1].strip()
+                
+                if (cCopi not in self.EscCharacters):
+                    self._strCopi += cCopi
+                else:
+                    cCopiEsc.append(len(self._strCopi))
+                    
+                if (cCipo not in self.EscCharacters):
+                    self._strCipo += cCipo
+                else:
+                    cCipoEsc.append(len(self._strCipo))
+    
+        ba = str(bytearray.fromhex(self._strCopi).decode())
+        for b in range(len(ba)):
+            if (len(cCopiEsc) > 0):
+                l = cCopiEsc[0]
+                while(b == l):
+                    cCopiEsc.pop(0)
+                    self._asciiCopi += r"\x"
+                    if (len(cCopiEsc) > 0):
+                        l = cCopiEsc[0]
+                    else:
+                        l = -1
+                
+            self._asciiCopi += ba[b]
+
+        ba = bytearray.fromhex(self._strCipo).decode()
+        for b in range(len(ba)):
+            if (len(cCipoEsc) > 0):
+                l = cCipoEsc[0]
+                while(b == l):
+                    cCipoEsc.pop(0)
+                    self._asciiCipo += r"\x"
+                    if (len(cCipoEsc) > 0):
+                        l = cCipoEsc[0]
+                    else:
+                        l = -1
+                
+            self._asciiCipo += ba[b]
+
+    def getCurrentLine(self):
+        return self._currentLine
+    
+    def getStrCopi(self):
+        return self._strCopi
+    
+    def getStrCipo(self):
+        return self._strCipo
+    
+    def getAsciiCopi(self):
+        return self._asciiCopi
+    
+    def getAsciiCipo(self):
+        return self._asciiCipo
+        
 
 def usage():
     print("%s : <c | p | cp> <filename.txt>" % (sys.argv[0], ))
     sys.exit(1)
 
 def main(filename, partFlag):
-    fh = io.open(filename, "r")
     fh2 = io.open(filename + "_parse.txt", "w")
-    lines = fh.readlines()
     i = 0
     
-    for line in lines:
-        strCopi = ""
-        strCipo = ""
+    adssp = AnalogDiscoverySpiSpyParser(filename)
+    
+    while(adssp.readCurrentLine()):
         i = i + 1
-        if rexData.match(line):
-            fh2.write("Line %d" % (i,))
-            fh2.write("\n")
-            dataParts = line.split(":")
-            lineParts = dataParts[1].split(",")
-            ioParts = []
-            for linePart in lineParts:
-                ioParts.append(linePart.split("|"))
+        if adssp.parseDataParts():            
+            adssp.getIoPartsAsAscii()
             
-            for ioPart in ioParts:
-                if (len(ioPart) == 2):
-                    cCopi = ioPart[0].strip()
-                    cCipo = ioPart[1].strip()
-                    
-                    if (cCopi not in EscCharacters):
-                        strCopi += cCopi
-                    # else:
-                    #     strCopi += "ESC"
-                        
-                    if (cCipo not in EscCharacters):
-                        strCipo += cCipo
-                    # else:
-                    #     strCipo += "ESC"
-            
-            if (partFlag in PartsCopi):
-                fh2.write(strCopi)
+            if (partFlag in adssp.PartsCopi):                
+                fh2.write(adssp.getStrCopi())
                 fh2.write("\n")
-                fh2.write(bytearray.fromhex(strCopi).decode())
+                fh2.write(adssp.getAsciiCopi())
                 fh2.write("\n")
-            if (partFlag in PartsCipo):
-                fh2.write(strCipo)
+
+            if (partFlag in adssp.PartsCipo):
+                fh2.write(adssp.getStrCipo())
                 fh2.write("\n")
-                fh2.write(bytearray.fromhex(strCipo).decode())
+                fh2.write(adssp.getAsciiCipo())
                 fh2.write("\n")
+
             fh2.write("\n")
             
-    fh.close()
+    adssp.close()
     fh2.close()
 
 if __name__ == "__main__":
-    if (len(sys.argv) > 2):
+    if (len(sys.argv) == 3):
         main(sys.argv[2], sys.argv[1])
+    elif (len(sys.argv) == 1):
+        partFlag = "c"
+        fileNames = ["SF-Tester-Design-AXI/CLS SPI Spy Capture of Boot-Time Display at ext_spi_clk SCK.txt",
+                     "SF-Tester-Design-AXI/CLS SPI Spy Capture of First-Iteration Display at ext_spi_clk SCK.txt",
+                     "SF-Tester-Design-VHDL/CLS SPI Spy Capture of Boot-Time Display at 50 KHz SCK.txt",
+                     "SF-Tester-Design-VHDL/CLS SPI Spy Capture of First-Iteration Display at 50 KHz SCK.txt"]
+        
+        for fileName in fileNames:
+            #try:
+                main(fileName, partFlag)
+            #except Exception as ex:
+            #    print("Exception raised {}, {} : {}".format(fileName, type(ex), str(ex)))
+            
     else:
         usage()
