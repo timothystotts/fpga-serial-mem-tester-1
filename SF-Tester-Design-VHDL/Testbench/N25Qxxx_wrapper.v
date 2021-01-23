2066abae8bb0efc478fd781203b9d7e7bec96bc9
Timothy Stotts <timothystotts08@gmail.com>
2021-01-22 21:27:05 -0500

    Beginnings of an OS-VVM test-bench for SF-Tester-Design-VHDL. The test-bench sources come from ACL-Tester-Design-Single-Clock-VHDL and require modification for Alert statements and UART/PmodCLS scoreboards to be applicable.

---
+++ SF-Tester-Design-VHDL/Testbench/N25Qxxx_wrapper.v
@@ -0,0 +1,76 @@
+/*------------------------------------------------------------------------------
+-- MIT License
+--
+-- Copyright (c) 2021 Timothy Stotts
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+------------------------------------------------------------------------------*/
+/**-----------------------------------------------------------------------------
+-- \file N25Qxxx_wrapper.v
+--
+-- \brief A Verilog-2001 wrapper around the Micro vendor module N25Qxxx.v , thus
+-- allowing this wrapper to be instantiated as a component inside of a
+-- VHDL test-bench.
+------------------------------------------------------------------------------*/
+//------------------------------------------------------------------------------
+//Part 1: Module header:--------------------------------------------------------
+module N25Qxxx_wrapper(
+	S, C, HOLD_DQ3, DQ0, DQ1, W_DQ2
+	);
+
+parameter time powerup_time = 150e0;
+
+input wire S;
+input wire C;
+inout wire HOLD_DQ3;
+inout wire DQ0;
+inout wire DQ1;
+inout wire W_DQ2;
+
+//Part 2: Declarations----------------------------------------------------------
+`define VoltageRange 31:0
+reg [`VoltageRange] Vcc;
+wire RESET2;
+
+//Part 3: Statements------------------------------------------------------------
+assign RESET2 = 1'b0;
+
+initial
+begin
+    `ifdef VCC_3V
+        Vcc = 'd3000;
+    `else
+        Vcc = 'd1800;
+    `endif    
+	#(powerup_time+100);
+end
+
+N25Qxxx #() u_N25Qxxx (
+	.S(S),
+	.C_(C),
+	.HOLD_DQ3(HOLD_DQ3),
+	.DQ0(DQ0),
+	.DQ1(DQ1),
+	.Vcc(Vcc),
+	.Vpp_W_DQ2(W_DQ2),
+	.RESET2(RESET2)
+	);
+
+endmodule
+//------------------------------------------------------------------------------
---
+++ SF-Tester-Design-VHDL/Work_Dir_RivPro/asim-compile-work.do
@@ -0,0 +1,50 @@
+set SRC_DIR ".."
+set ACOM_OPTS "-2008 -dbg"
+set ALOG_OPTS "-v2k5 -dbg"
+set MTI_ALOG_OPTS "-v2k5 -incdir ${SRC_DIR}/N25Q256A13E_VG12"
+
+amap unisim "C:\\Aldec\\Xilinx_lib_2020.2\\unisim"
+amap unimacro "C:\\Aldec\\Xilinx_lib_2020.2\\unimacro"
+
+adel -lib work -all
+alib work
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/clock_divider.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/clock_enable_divider.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/arty_reset_synchronizer.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/lcd_text_functions_pkg.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/multi_input_debounce.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pulse_stretcher_synch.vhdl
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/sf_tester_fsm.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/sf_testing_to_ascii.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/lcd_text_feed.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/led_pwm_driver.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/led_palette_updater.vhdl
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pmod_generic_spi_solo.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pmod_generic_qspi_solo.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pmod_sf3_quad_spi_solo.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pmod_cls_stand_spi_solo.vhdl
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pmod_sf3_custom_driver.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/pmod_cls_custom_driver.vhdl
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/uart_tx_feed.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/uart_tx_only.vhdl
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/RTL/fpga_serial_mem_tester.vhdl
+
+eval alog ${MTI_ALOG_OPTS} -work work ${SRC_DIR}/N25Q256A13E_VG12/code/N25Qxxx.v
+
+eval alog ${MTI_ALOG_OPTS} -work work ${SRC_DIR}/Testbench/N25Qxxx_wrapper.v
+
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/sf3_testbench_pkg.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/clock_gen.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/board_ui.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/pmod_sf3.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/board_uart.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/pmod_cls.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/fpga_serial_mem_tester_testbench.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/fpga_serial_mem_tester_testharness.vhdl
+eval acom ${ACOM_OPTS} -work work ${SRC_DIR}/Testbench/test_default_fpga_regression.vhdl
